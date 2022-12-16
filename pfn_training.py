a236e9d1f9d1fbe18f1e2b7e2e643046828e1ca8
bk806714 <bk806714@ohio.edu>
2022-12-16 12:42:43 -0800

    pfn hcal insert training

--- pfn_training.py
+++ pfn_training.py
@@ -1,49 +1,68 @@
 import tensorflow as tf
 from energyflow.archs import PFN
 from training_functions import *
+from sklearn.preprocessing import StandardScaler
 import h5py as h5
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import shutil
-import pickle
+import time
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+gpus = tf.config.experimental.list_physical_devices('GPU')
+#print(gpus)
+particle='pi+'
+rate=5e-3
+for gpu in gpus:
+    print("Name:", gpu.name, "  Type:", gpu.device_type)
 
-print(tf.config.experimental.list_physical_devices('GPU'))
+mirrored_strategy = tf.distribute.MirroredStrategy()
+start_time=time.time()
 
-import yaml
+if particle=='e-':
+    h5_filename = '/media/miguel/Elements/Data_hcali/Data1/e-_623k_standalone_0_80GeV_train_split.hdf5'
+    new_dir=f"standalone_model_623k_{particle}_1k_{rate}"
+elif particle=='pi+':
+    h5_filename = '/media/miguel/Elements/Data_hcali/Data1/pi+_495k_0_100GeV_train_split.hdf5'
+    new_dir=f"aaastandalone_model_495k_{particle}_1k_{rate}"
+else:
+    print('WHAT IS PARTICLE')
 
-#Using YAML configuration File
-with open("config.yaml", "r") as config_file:
-    config = yaml.safe_load(config_file)
 
-h5_file_name = config["h5_file_name"]
-learning_rate = config["learning_rate"]
-num_epochs = config["n_epochs"]
-batch_size = config["batch_size"]
-
-h5_filename = "2M_uncompressed.hdf5"
+#h5_filename = "/media/miguel/Elements/Data_hcali/Data1/total_train_split_1290k_pi+_gt50.hdf5"
+#h5_filename = "/media/miguel/Elements/Data_hcali/Data1/total_train_split_1290k_pi+_lt50.hdf5"
+#h5_filename = "/media/miguel/Elements/Data_hcali/Data1/total_train_split_300k_pi+.hdf5" 
+#h5_filename = "/media/miguel/Elements/Data_hcali/Data1/total_train_split_658k_e-.hdf5"
+#h5_filename ='/media/miguel/Elements/Data_hcali/Data1/e-_623k_standalone_0_80GeV_train_split.hdf5'
+#h5_filename ='/media/miguel/Elements/Data_hcali/Data1/pi+_495k_0_100GeV_train_split.hdf5'
+#h5_filename = "/media/miguel/Elements/Data_hcali/Data1/pi+_80k_standalone_train_split.hdf5"
 h5_file = h5.File(h5_filename,'r')
-
-label = "MAELoss_NoTarget_Norm_50E"  #Replace with your own variation!      
+working_dir=os.getcwd()
+#new_dir=f"standalone_model_80k_pi+_1k_{rate}"
+try:
+    os.makedirs(f"{working_dir}/{new_dir}",exist_ok=True)
+except OSError:
+    print("Directory creation Error "%new_dir)
+else:
+    print("Directory %s is created"%new_dir)
+label = new_dir  #Replace with your own variation!      
 path = "./"+label
-shutil.rmtree(path, ignore_errors=True)
-os.makedirs(path)
 
 
-do_normalization = True
-input_dim = h5_file['train_hcal'].shape[-2] #should be 4: Cell E,X,Y,Z, the number of features per particle
-learning_rate = 1e-3
-dropout_rate = 0.1
-batch_size = 1_000
-N_Epochs = 50
-patience = 10
+input_dim = h5_file['train_hcali'].shape[-2] #should be 4: Cell E,X,Y,Z, the number of features per particle
+learning_rate =rate
+dropout_rate = 0.05
+batch_size = 1000
+N_Epochs = 400
+patience = 20
 N_Latent = 128
 shuffle_split = True #Turn FALSE for images!
 train_shuffle = True #Turn TRUE for images!
-loss = 'mae' #'mae'
+Y_scalar = True
+loss = 'mse' #'mae' and mse are two options #'swish'
 
 Phi_sizes, F_sizes = (100, 100, N_Latent), (100, 100, 100)
 output_act, output_dim = 'linear', 1 #Train to predict error
-
 
 pfn = PFN(input_dim=input_dim, 
           Phi_sizes=Phi_sizes, 
@@ -55,63 +74,45 @@ pfn = PFN(input_dim=input_dim,
           F_dropouts=dropout_rate,
           optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate))
 
-
 # Tensorflow CallBacks
 lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lr_decay,verbose=0)
 early_stopping = tf.keras.callbacks.EarlyStopping(patience=patience)
 history_logger=tf.keras.callbacks.CSVLogger(path+"/log.csv", separator=",", append=True)
-model_checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=path, save_best_only=True)
-callbacks=[lr_scheduler, early_stopping,history_logger,batch_history(),model_checkpoint]
+model_checkpoint = tf.keras.callbacks.ModelCheckpoint( filepath=path, save_best_only=True)
 
 
 train_generator = tf.data.Dataset.from_generator(
-    training_generator(h5_filename,'train_hcal','train_mc',batch_size,do_normalization,path),
+    training_generator(h5_filename,'train_hcali','train_mc',batch_size),
     output_shapes=(tf.TensorShape([None,None,None]),[None]),
     output_types=(tf.float64, tf.float64))
 
-# train_generator = tf.data.Dataset.from_generator(
-#     training_generator(h5_filename,'train_hcal','train_mc',batch_size,do_normalization,path),
-#     tf.TensorSpec(shape=((None,None,None),(None)), dtype=tf.float64),
-#     output_types=(tf.float64, tf.float64))
 
 val_generator = tf.data.Dataset.from_generator(
-    training_generator(h5_filename,'val_hcal','val_mc',batch_size,do_normalization,path),
+    training_generator(h5_filename,'val_hcali','val_mc',batch_size),
     output_shapes=(tf.TensorShape([None,None,None]),[None]),
     output_types=(tf.float64, tf.float64))
 
 test_generator = tf.data.Dataset.from_generator(
-    test_generator(h5_filename,'test_hcal','test_mc',batch_size,do_normalization,path),
+    test_generator(h5_filename,'test_hcali','test_mc',batch_size),
     output_shapes=(tf.TensorShape([None,None,None])),
     output_types=(tf.float64))
 
-
-N_QA_Batches = 5 #number of batches for just plotting input data
-pre_training_QA(h5_filename,path,N_QA_Batches,batch_size,do_normalization)
-
-history = pfn.fit(
+# training_generator.batch(batch_size)
+# val_generator.batch(batch_size)
+# test_generator.batch(batch_size)
+the_fit = pfn.fit(
     train_generator,
     epochs=N_Epochs,
     batch_size=batch_size,
-    callbacks=callbacks,
+    callbacks=[lr_scheduler, early_stopping,history_logger,model_checkpoint],
     validation_data=val_generator,
     verbose=1
 )
 
-
-#save batch loss
-np.save("%s/batch_loss.npy"%(path),batch_history.batch_loss)
-
-#save epoch loss
-with open(path+'/history_file', 'wb') as hist_file:
-    pickle.dump(history.history, hist_file)
-
 pfn.layers
 pfn.save("%s/energy_regression.h5"%(path))
 mypreds = pfn.predict(test_generator, batch_size=1000)
+#mypreds=target_stdevs*mypreds_temp + target_means
 np.save("%s/predictions.npy"%(path),mypreds)
 #FIXME: un-norm the predictions
-
-
-
-
 
